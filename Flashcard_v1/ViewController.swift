d9e73200b861aadf7ea26e3d9b0a3440f0e51e98
yyang <yoyang@pvamu.edu>
2022-03-23 19:10:53 -0500

    Initial Commit

---
+++ Flashcard_v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard_v1
+//
+//  Created by Yonggao Yang on 3/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
