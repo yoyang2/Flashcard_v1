d8d158fcd3fa8faf4d2d89d2649d2a82389ad155
yyang <yoyang@pvamu.edu>
2022-03-28 18:26:00 -0500

    Build the flashcards; Finish the initialization; adjust the font size of the two labels.

--- Flashcard_v1/ViewController.swift
+++ Flashcard_v1/ViewController.swift
@@ -8,17 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var qLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    
+    var questions: [String] = []    //used to store all the questions (strings)
+    var answers: [String] = []      //used to store all the answers (strings)
+    var cardIndex = 0          //pointing to the 1st card
+    
+    override func viewDidLoad() {//the starting point of the app
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        LoadQuestionsAnswers() //call this function to build the stack of flashcards
+        qLabel.text = questions[cardIndex]
+        aLabel.text = "???"
     }
-
 
     //function for loading questions and answers to the flashcards
     func LoadQuestionsAnswers() {
+        questions.append("Which city is the capital of Japan?")
+        answers.append("Tokyo")
+        questions.append("2 to power of 10 = ?")
+        answers.append("1024")
+        questions.append("Who is the current Governor of TX?")
+        answers.append("Greg Abbott")
+        //.......
+    }
+    
+    @IBAction func nextQAction(_ sender: Any) {
+
+    }
+    
+    @IBAction func showAAction(_ sender: Any) {
         
     }
+    
     
 }
 
